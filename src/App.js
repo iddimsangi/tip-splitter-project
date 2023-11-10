cff008ec4fa0ea16ad5dbb468fea521b0a1be28a
iddimsangi <adammsangi@gmail.com>
2023-11-10 09:43:01 +0300

    header and selection tip sections

--- src/App.js
+++ src/App.js
@@ -3,26 +3,55 @@ function App() {
   return (
     <di>
       <div className="bg-[#C3E5E9] h-screen flex flex-col justify-center items-center p-5">
-        <h1 className="text-[#00484C] uppercase font-bold  ">
+        <h1 className="text-[#00484C] uppercase font-mono font-extrabold tracking-wide">
           Spli <span className="block">tter</span>
         </h1>
-        <div className="bg-white p-3 mt-3  rounded shadow-lg min-w-full flex flex-col justify-center items-center space-y-3">
+        <div className="bg-white p-6 mt-3  rounded shadow-lg min-w-full flex flex-col justify-center items-center space-y-3">
           <div className=" bg-red-700 w-full p-1 flex flex-col space-y-2">
-            <form className="flex flex-col bg-blue-500">
-              <label>Bill</label>
-              <div className="flex">
-                <img src={dollar} alt="dollar" />
-                <input type="number" value={889} className="w-full" />
+            <form className="flex flex-col">
+              <label className=" font-mono text-very-dark-cyan">Bill</label>
+              <div className="flex items-center bg-[#F3F8FB] p-2 rounded">
+                <img src={dollar} className=" ml-3" alt="dollar" />
+                <input
+                  type="text"
+                  value={889}
+                  className="w-full bg-transparent font-extrabold text-xl text-right font-mono text-very-dark-cyan outline-none"
+                />
               </div>
             </form>
             <div className=" bg-orange-700 w-full ">
-              <h2>Select Tip %</h2>
-              <div className=" grid grid-cols-2 grid-rows-3">
-                <a href="#">5%</a>
-                <a href="#">10%</a>
-                <a href="#">15%</a>
-                <a href="#">25%</a>
-                <a href="#">50%</a>
+              <h2 className=" font-mono text-very-dark-cyan">Select Tip %</h2>
+              <div className=" grid grid-cols-2 grid-rows-3 gap-3">
+                <a
+                  className=" bg-very-dark-cyan  flex justify-center items-center px-12 text-white mb-1 font-mono font-bold rounded-md py-2"
+                  href="#"
+                >
+                  5%
+                </a>
+                <a
+                  className=" bg-very-dark-cyan  flex justify-center items-center px-12 text-white mb-1 font-mono font-bold rounded-md py-2"
+                  href="#"
+                >
+                  10%
+                </a>
+                <a
+                  className=" bg-very-dark-cyan  flex justify-center items-center px-12 text-white mb-1 font-mono font-bold rounded-md py-2"
+                  href="#"
+                >
+                  15%
+                </a>
+                <a
+                  className=" bg-very-dark-cyan  flex justify-center items-center px-12 text-white mb-1 font-mono font-bold rounded-md py-2"
+                  href="#"
+                >
+                  25%
+                </a>
+                <a
+                  className=" bg-very-dark-cyan  flex justify-center items-center px-12 text-white mb-1 font-mono font-bold rounded-md py-2"
+                  href="#"
+                >
+                  50%
+                </a>
                 {/* <button>5%</button>
                 <button>10%</button>
                 <button>15%</button>
